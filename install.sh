e503f2a4f9da0f97f36e7cdbd747bfa989cb1d23
Todd Treece <toddtreece@gmail.com>
2015-07-15 13:46:04 -0400

    remove the need for sudo

--- install.sh
+++ install.sh
@@ -5,9 +5,9 @@ sleep 3
 export DISPLAY=:1.0
 wget http://downloads.arduino.cc/arduino-1.6.5-linux64.tar.xz
 tar xf arduino-1.6.5-linux64.tar.xz
-sudo mv arduino-1.6.5 /usr/local/share/arduino
-sudo ln -s /usr/local/share/arduino/arduino /usr/local/bin/arduino
-ln -s $PWD /usr/local/share/arduino/libraries/Adafruit_Test_Library
+mv arduino-1.6.5 $HOME/arduino
+ln -s $PWD $HOME/arduino/libraries/Adafruit_Test_Library
+export PATH="$HOME/arduino:$PATH"
 arduino --pref "boardsmanager.additional.urls=http://arduino.esp8266.com/package_esp8266com_index.json" --save-prefs
 arduino --install-boards arduino:sam > /dev/null
 arduino --install-boards esp8266:esp8266 > /dev/null
