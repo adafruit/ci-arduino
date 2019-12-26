b641400704fb690057dace6912ca2e8656e4c51d
Lady Ada <limor@ladyada.net>
2019-12-26 02:07:03 -0500

    use argv

--- arduino_install.py
+++ arduino_install.py
@@ -9,28 +9,6 @@ from clint.textui import colored
 # add user bin to path!
 os.environ["PATH"] += os.pathsep + os.environ["TRAVIS_BUILD_DIR"] + "/bin"
 
-ALL_PLATFORMS={
-    # classic Arduino AVR
-    "uno" : "arduino:avr:uno",
-    "leonardo" : "arduino:avr:leonardo",
-    "mega2560" : "arduino:avr:mega:cpu=atmega2560",
-    # Arduino SAMD
-    "zero" : "arduino:samd:arduino_zero_native",
-    "cplayExpress" : "arduino:samd:adafruit_circuitplayground_m0",
-    # Espressif
-    "esp8266" : "esp8266:esp8266:huzzah:eesz=4M3M,xtal=80",
-    "esp32" : "esp32:esp32:featheresp32:FlashFreq=80",
-    # Adafruit AVR
-    "trinket" : "adafruit:avr:trinket5",
-    "gemma" : "arduino:avr:gemma",
-    "cplayClassic" : "arduino:avr:circuitplay32u4cat",
-    # Adafruit SAMD
-    "m4" : "adafruit:samd:adafruit_metro_m4:speed=120",
-    "cplayExpressAda" : "adafruit:samd:adafruit_circuitplayground_m0",
-    # Adafruit nRF
-    "cplayBluefruit" : "adafruit:nrf52:cplaynrf52840:softdevice=s140v6,debug=l0",
-}
-#print(ALL_PLATFORMS)
 ########################################################################
 print()
 print('#'*40)
