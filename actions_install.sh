b3034f1cecfa5c11035603b4115a0e5bc81225a3
Lady Ada <limor@ladyada.net>
2019-12-27 16:12:00 -0500

    ok nvmd

--- actions_install.sh
+++ actions_install.sh
@@ -3,5 +3,5 @@
 set -e
 
 pip3 install clint pyserial setuptools adafruit-nrfutil
-sudo apt install -fy cppcheck clang-format-9
-sudo ln -s /usr/bin/clang-format-9 /usr/bin/clang-format
+sudo apt install -fy cppcheck clang-format-8
+sudo ln -s /usr/bin/clang-format-8 /usr/bin/clang-format
