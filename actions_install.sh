ed2d0da596a1338427d35146d0ca005e522d35de
Lady Ada <limor@ladyada.net>
2019-12-27 15:58:20 -0500

    bump to 10

--- actions_install.sh
+++ actions_install.sh
@@ -3,5 +3,5 @@
 set -e
 
 pip3 install clint pyserial setuptools adafruit-nrfutil
-sudo apt install -fy cppcheck clang-format-8
-sudo ln -s /usr/bin/clang-format-8 /usr/bin/clang-format
+sudo apt install -fy cppcheck clang-format-10
+sudo ln -s /usr/bin/clang-format-10 /usr/bin/clang-format
