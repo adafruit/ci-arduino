38d0ee8832904ddf33a230e1d3c7dbd122527dfd
Lady Ada <limor@ladyada.net>
2019-12-27 13:57:38 -0500

    add adafruit deps

--- actions_install.sh
+++ actions_install.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-pip3 install clint pyserial
+pip3 install clint pyserial setuptools adafruit-nrfutil
 sudo apt install -fy cppcheck clang-format-8
 sudo ln -s /usr/bin/clang-format-8 /usr/bin/clang-format
