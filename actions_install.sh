1ef3bbd8eb9199cb035e3f09a57f66ff02ae41bb
Lady Ada <limor@ladyada.net>
2019-12-27 16:11:15 -0500

    maybe 9 is available?

--- actions_install.sh
+++ actions_install.sh
@@ -3,5 +3,5 @@
 set -e
 
 pip3 install clint pyserial setuptools adafruit-nrfutil
-sudo apt install -fy cppcheck clang-format-10
-sudo ln -s /usr/bin/clang-format-10 /usr/bin/clang-format
+sudo apt install -fy cppcheck clang-format-9
+sudo ln -s /usr/bin/clang-format-9 /usr/bin/clang-format
