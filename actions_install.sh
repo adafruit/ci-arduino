4fe206d3b086a80236b9f016ca4da1000914d963
Lady Ada <limor@ladyada.net>
2019-12-27 22:51:37 -0500

    make subdirs earlier

--- actions_install.sh
+++ actions_install.sh
@@ -5,3 +5,9 @@ set -e
 pip3 install clint pyserial setuptools adafruit-nrfutil
 sudo apt install -fy cppcheck clang-format-8
 sudo ln -s /usr/bin/clang-format-8 /usr/bin/clang-format
+
+# make all our directories we need for files and libraries
+mkdir ${HOME}/.arduino15
+mkdir ${HOME}/.arduino15/packages
+mkdir ${HOME}/Arduino
+mkdir ${HOME}/Arduino/libraries
