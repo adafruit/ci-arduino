626585c807e7f596487aeaf58eeeffe711da1bdc
hathach <thach@tinyusb.org>
2019-12-30 23:07:12 +0700

    revert tinyusb_platforms

--- build_platform.py
+++ build_platform.py
@@ -62,8 +62,7 @@ ALL_PLATFORMS={
     "main_platforms" : ("uno", "leonardo", "mega2560", "zero",
                         "esp8266", "esp32", "metro_m4", "nrf52840"),
     "arcada_platforms" : ("pybadge", "pygamer", "hallowing_m4",
-                          "cpb", "cpx_ada"),
-    "tinyusb_platforms" : ("metro_m0_tinyusb", "metro_m4_tinyusb", "nrf52840")
+                          "cpb", "cpx_ada")
 }
 
 BSP_URLS = "https://adafruit.github.io/arduino-board-index/package_adafruit_index.json,http://arduino.esp8266.com/stable/package_esp8266com_index.json,https://dl.espressif.com/dl/package_esp32_index.json,https://sandeepmistry.github.io/arduino-nRF5/package_nRF5_boards_index.json"
