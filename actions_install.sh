ecf38d79652df0ea0ac7345dad199626f2024797
Lady Ada <limor@ladyada.net>
2020-01-11 12:47:58 -0500

    maybe needed?

--- actions_install.sh
+++ actions_install.sh
@@ -3,6 +3,7 @@
 set -e
 
 pip3 install clint pyserial setuptools adafruit-nrfutil
+sudo apt-get update
 sudo apt install -fy cppcheck clang-format-8
 sudo ln -s /usr/bin/clang-format-8 /usr/bin/clang-format
 
