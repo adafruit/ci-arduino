eee2ae803c4a3a2a3e84edb938720c904be90abd
Dimitris Platis <platisd@users.noreply.github.com>
2020-01-28 21:27:52 +0100

    Use the latest Arduino IDE version
    As of today, the latest Arduino IDE version is 1.8.11 which includes
    several bug-fixes and enhancements.

--- install.sh
+++ install.sh
@@ -43,7 +43,7 @@ echo "########################################################################";
 
 # if .travis.yml does not set version
 if [ -z $ARDUINO_IDE_VERSION ]; then
-export ARDUINO_IDE_VERSION="1.8.10"
+export ARDUINO_IDE_VERSION="1.8.11"
 echo "NOTE: YOUR .TRAVIS.YML DOES NOT SPECIFY ARDUINO IDE VERSION, USING $ARDUINO_IDE_VERSION"
 fi
 
