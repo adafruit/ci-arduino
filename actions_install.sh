2ef0a08c8b1d7605d39e91ea4328f8ad37df0a58
lady ada <limor@ladyada.net>
2020-10-18 18:45:15 -0400

    revert

--- actions_install.sh
+++ actions_install.sh
@@ -2,16 +2,13 @@
 
 set -e
 
-sudo apt-get update
-sudo apt-get remove python3 python
-sudo apt-get install python3.8
-
 pip3 install clint pyserial setuptools adafruit-nrfutil
+sudo apt-get update
 sudo apt-get install -y --allow-downgrades libllvm8=1:8-3~ubuntu18.04.2 -V
 sudo apt install -fy cppcheck clang-format-8
-#if [ ! -f /usr/bin/clang-format ]; then
-#    sudo ln -s /usr/bin/clang-format-8 /usr/bin/clang-format
-#fi
+if [ ! -f /usr/bin/clang-format ]; then
+    sudo ln -s /usr/bin/clang-format-8 /usr/bin/clang-format
+fi
 
 # make all our directories we need for files and libraries
 mkdir ${HOME}/.arduino15
