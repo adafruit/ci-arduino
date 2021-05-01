65a4580800f0211d09b0deafeccfc4c5a3600fc1
lady ada <limor@ladyada.net>
2021-05-01 13:18:59 -0400

    wheel around

--- install.sh
+++ install.sh
@@ -139,14 +139,14 @@ fi
 
 if [[ $INSTALL_NRF52 == 1 ]]; then
   echo -n "ADAFRUIT NRF5X: "
+  pip3 install --user wheel
   pip3 install --user setuptools
   pip3 install --user adafruit-nrfutil
   pip3 install --user pyserial
-  pip3 install --user wheel
+  sudo pip3 install wheel
   sudo pip3 install setuptools
   sudo pip3 install adafruit-nrfutil
   sudo pip3 install pyserial
-  sudo pip3 install wheel
   DEPENDENCY_OUTPUT=$(arduino --install-boards adafruit:nrf52 2>&1)
   if [ $? -ne 0 ]; then echo -e "\xe2\x9c\x96 OR CACHED"; else echo -e """$GREEN""\xe2\x9c\x93"; fi
 fi
