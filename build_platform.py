4845cdfa8ea0e9c601785cd037b2fd9d8e77ef9b
hathach <thach@tinyusb.org>
2021-09-29 00:21:34 +0700

    increase timeout to 3 mins, esp32s2 example can take long time to build

--- build_platform.py
+++ build_platform.py
@@ -318,7 +318,7 @@ def test_examples_in_folder(folderpath):
             cmd = ['arduino-cli', 'compile', '--warnings', 'none', '--export-binaries', '--fqbn', fqbn, examplepath]
         proc = subprocess.Popen(cmd, stdout=subprocess.PIPE,
                                 stderr=subprocess.PIPE)
-        r = proc.wait(timeout=60)
+        r = proc.wait(timeout=60*3)
         out = proc.stdout.read()
         err = proc.stderr.read()
         if r == 0 and not (err and BUILD_WALL == True):
