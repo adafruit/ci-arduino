8dda6941f5da9abdc82e6599ed3751f3c1f9a020
hathach <thach@tinyusb.org>
2021-10-07 20:45:34 +0700

    test

--- build_platform.py
+++ build_platform.py
@@ -335,13 +335,13 @@ def test_examples_in_folder(folderpath):
 
         proc = subprocess.Popen(cmd, stdout=subprocess.PIPE,
                                 stderr=subprocess.PIPE)
-        r = proc.wait(timeout=60*5)
+        r = proc.wait(timeout=60)
         out = proc.stdout.read()
         err = proc.stderr.read()
-        #print(cmd)
-        #print(r)
-        #ColorPrint.print_info(out.decode("utf-8"))
-        #ColorPrint.print_info(err.decode("utf-8"))
+        print(cmd)
+        print(r)
+        ColorPrint.print_info(out.decode("utf-8"))
+        ColorPrint.print_info(err.decode("utf-8"))
         if r == 0 and not (err and BUILD_WALL == True):
             ColorPrint.print_pass(CHECK)
             if err:
