88ab7a795edbf7b7b4a1ed075064e5d5f7e10763
Jeff Epler <jepler@gmail.com>
2022-02-11 10:55:16 -0600

    Remove the whole blink example folder
    This started failing in the Learning System Guides when I added
    .generate files.

--- build_platform.py
+++ build_platform.py
@@ -40,8 +40,7 @@ IS_LEARNING_SYS = False
 if "Adafruit_Learning_System_Guides" in BUILD_DIR:
     print("Found learning system repo")
     IS_LEARNING_SYS = True
-    os.remove(BUILD_DIR + "/ci/examples/Blink/Blink.ino")
-    os.rmdir(BUILD_DIR + "/ci/examples/Blink")
+    shutil.rmtree(BUILD_DIR + "/ci/examples/Blink")
 elif "METROX-Examples-and-Project-Sketches" in BUILD_DIR:
     print("Found MetroX Examples Repo")
     IS_LEARNING_SYS = True
