2de2cd8f259ff284119bf999e22401b528fd1a08
brentru <brent@adafruit.com>
2022-03-07 15:07:28 -0500

    use better names for espressif platforms

--- build_platform.py
+++ build_platform.py
@@ -60,8 +60,8 @@ ALL_PLATFORMS={
     "zero" : ["arduino:samd:arduino_zero_native", "0x68ed2b88"],
     "cpx" : ["arduino:samd:adafruit_circuitplayground_m0", "0x68ed2b88"],
     # Espressif
-    "esp8266" : ["esp8266:esp8266:huzzah:eesz=4M3M,xtal=80", None],
-    "esp32" : ["esp32:esp32:featheresp32:FlashFreq=80", None],
+    "feather_esp8266" : ["esp8266:esp8266:huzzah:eesz=4M3M,xtal=80", None],
+    "feather_esp32" : ["esp32:esp32:featheresp32:FlashFreq=80", None],
     "feather_esp32_v2_daily" : ["espressif:esp32:adafruit_feather_esp32_v2", None],
     "magtag" : ["esp32:esp32:adafruit_magtag29_esp32s2", "0xbfdd4eee"],
     "funhouse" : ["esp32:esp32:adafruit_funhouse_esp32s2", "0xbfdd4eee"],
