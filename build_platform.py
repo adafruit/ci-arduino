ffa44170ce58eb1a1454968a8704a6f825043f46
Paint Your Dragon <paintyourdragon@dslextreme.com>
2022-07-07 17:48:53 -0700

    Remove feather_esp32s3_tinyusb (not a valid combo)

--- build_platform.py
+++ build_platform.py
@@ -72,7 +72,6 @@ ALL_PLATFORMS={
     "feather_esp32s2" : ["esp32:esp32:adafruit_feather_esp32s2", "0xbfdd4eee", None],
     "feather_esp32s2_tft" : ["esp32:esp32:adafruit_feather_esp32s2_tft", "0xbfdd4eee", None],
     "feather_esp32s3" : ["esp32:esp32:adafruit_feather_esp32s3_nopsram", "0xc47e5767", None],
-    "feather_esp32s3_tinyusb" : ["esp32:esp32:adafruit_feather_esp32s3_nopsram:usbstack=tinyusb", "0xc47e5767", None],
     "feather_esp32s3_4mbflash_2mbpsram" : ["esp32:esp32:adafruit_feather_esp32s3", "0xc47e5767", None],
     "feather_esp32s3_tft" : ["esp32:esp32:adafruit_feather_esp32s3_tft", "0xc47e5767", None],
     "qtpy_esp32s3" : ["esp32:esp32:adafruit_qtpy_esp32s3", "0xc47e5767", None],
