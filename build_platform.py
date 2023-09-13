f28f8bc2bf692c9fedaf3e2907b533bc34815ebe
Tyeth Gundry <tyethgundry@googlemail.com>
2023-09-13 16:07:38 +0100

    Remove timeout value from sys.argv as well

--- build_platform.py
+++ build_platform.py
@@ -24,6 +24,7 @@ BUILD_TIMEOUT = False
 if "--build_timeout" in sys.argv:
     BUILD_TIMEOUT = True
     popen_timeout = sys.argv[sys.argv.index("--build_timeout") + 1]
+    sys.argv.pop(sys.argv.index("--build_timeout") + 1)
     sys.argv.remove("--build_timeout")
 
 
