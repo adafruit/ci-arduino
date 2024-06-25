bb8dc84cbce1293bb1346ee431ee4dd48d52b7bd
Liz <23021834+BlitzCityDIY@users.noreply.github.com>
2024-06-25 10:49:10 -0400

    edit trrs trinkey
    curious if TRRStrinkey vs trrstrinkey is what is causing the variant file to not be found on learn

--- all_platforms.py
+++ all_platforms.py
@@ -92,7 +92,7 @@ ALL_PLATFORMS={
     "neokeytrinkey_m0" : ["adafruit:samd:adafruit_neokeytrinkey_m0", "0x68ed2b88", None],
     "slidetrinkey_m0" : ["adafruit:samd:adafruit_slidetrinkey_m0", "0x68ed2b88", None],
     "proxlighttrinkey_m0" : ["adafruit:samd:adafruit_proxlighttrinkey_m0", "0x68ed2b88", None],
-    "trrstrinkey_m0" : ["adafruit:samd:adafruit_TRRStrinkey_m0", "0x68ed2b88", None],
+    "trrstrinkey_m0" : ["adafruit:samd:adafruit_trrstrinkey_m0", "0x68ed2b88", None],
     "thumbsticktrinkey_m0" : ["adafruit:samd:adafruit_thumbsticktrinkey_m0", "0x68ed2b88", None],
     "sht4xtrinkey_m0" : ["adafruit:samd:adafruit_sht4xtrinkey_m0", "0x68ed2b88", None],
     "pixeltrinkey_m0" : ["adafruit:samd:adafruit_pixeltrinkey_m0", "0x68ed2b88", None],
