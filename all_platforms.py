78afecb10412b67544ea4471773b30c80d1c27f5
Tyeth Gundry <tyethgundry@googlemail.com>
2026-06-24 14:56:09 +0100

    Add lilygo_t_display_s3 configuration

--- all_platforms.py
+++ all_platforms.py
@@ -46,6 +46,7 @@ ALL_PLATFORMS={
     "esp32s3_devkitc_1_n8_debug" : ["espressif:esp32:esp32s3:FlashSize=8M,PartitionScheme=noota_ffat,FlashMode=qio,PSRAM=disabled,DebugLevel=verbose,USBMode=default,CDCOnBoot=cdc,LoopCore=1,EventsCore=1", "0xc47e5767", "adafruit/bsp-3-3-10"],
     "feather_esp32s3" : ["espressif:esp32:adafruit_feather_esp32s3_nopsram", "0xc47e5767", "adafruit/bsp-3-3-10"],
     "wippersnapper_feather_esp32s3" : ["espressif:esp32:adafruit_feather_esp32s3_nopsram:PartitionScheme=tinyuf2_noota", "0xc47e5767", "adafruit/bsp-3-3-10"],
+    "lilygo_t_display_s3" : ["espressif:esp32:lilygo_t_display_s3:PartitionScheme=tinyuf2_noota", "0xc47e5767", "adafruit/bsp-3-3-10"],
     "feather_esp32s3_debug" : ["espressif:esp32:adafruit_feather_esp32s3_nopsram:DebugLevel=verbose,PartitionScheme=tinyuf2_noota", "0xc47e5767", "adafruit/bsp-3-3-10"],
     "wippersnapper_feather_esp32s3_4mbflash_2mbpsram" : ["espressif:esp32:adafruit_feather_esp32s3:PartitionScheme=tinyuf2_noota", "0xc47e5767", "adafruit/bsp-3-3-10"],
     "feather_esp32s3_4mbflash_2mbpsram" : ["espressif:esp32:adafruit_feather_esp32s3:PartitionScheme=tinyuf2_noota", "0xc47e5767", "adafruit/bsp-3-3-10"],
